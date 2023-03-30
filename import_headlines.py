162038953bf117826571d6707adb5407e3feffc0
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-30 15:45:07 +0100

    Initial Commit

---
+++ import_headlines.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+import string
+from nltk.corpus import stopwords
+
+STOPWORDS = stopwords.words('english') + ['u', 'ü', 'ur', '4', '2', 'im', 'dont', 'doin', 'ure']
+
+def text_process(mess):
+    """
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Returns a list of the cleaned text
+    """
+    
+    # Check characters to see if they are in punctuation
+    nopunc = [char for char in mess if char not in string.punctuation]
+    
+    # Join the characters again to form the string.
+    nopunc = ''.join(nopunc)
+    # Now just remove any stopwords
+    return ' '.join([word for word in nopunc.split() if word.lower() not in STOPWORDS])
+
+def get_headlines(fileName : str):
+    news = pd.read_csv("kaggle/input/million-headlines/abcnews-date-text.csv")
+    news["publish_date"] = pd.to_datetime(news["publish_date"].astype(str),infer_datetime_format=True)
+    
+    news["headline_text"] = news.headline_text.apply(text_process)
+    news = news.groupby("publish_date")["headline_text"].agg(' '.join).to_frame()
+    news.index.names = ["date"]
+
+    return news
+
---
+++ import_stocks.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+def do_folder(folderName: str):
+    bigStockMerge = pd.DataFrame()
+    for dirname, _, filenames in os.walk('/kaggle/input/nasdaq-daily-stock-prices/'):
+        for filename in filenames:
+            
+            thisFile = os.path.join(dirname, filename)
+            bigStockMerge = import_one(thisFile, bigStockMerge)
+            
+    
+    bigStockMerge["Average"] = bigStockMerge.mean(axis=1)
+    bigStockMerge.index = pd.to_datetime(bigStockMerge.index)
+    bigStockMerge = bigStockMerge[[c for c in bigStockMerge.columns if c in {"date","Average"}]]
+    return bigStockMerge
+
+
+def import_one(fileName : str, targetFrame : pd.DataFrame):
+    stock_data = pd.read_csv(fileName,index_col='date')
+    ticker = stock_data["ticker"][0]
+    stock_data[ticker] = (stock_data["close"] - stock_data["open"])/stock_data["open"]
+    
+    trimmed_stock = stock_data[[c for c in stock_data.columns if c in {"date",ticker}]]
+    if targetFrame.size > 0:
+        return targetFrame.merge(trimmed_stock,how="outer", on="date")
+    else:
+        return trimmed_stock
