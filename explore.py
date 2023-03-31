0a8531c78d5313b9735369c68c9bbfe500d3fc33
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-31 14:48:54 +0100

    updated exploration created visualisation notebook

--- explore.py
+++ explore.py
@@ -41,7 +41,7 @@ final_data = final_data[[c for c in final_data.columns if c in {"date","headline
 print("Vectorizing")
 # instantiate the vectorizer
 vect = CountVectorizer()
-vect.fit(final_data["headline_text"])
+news_vector = vect.fit(final_data["headline_text"])
 
 
 # fit and transform
@@ -50,7 +50,7 @@ news_vector = vect.fit_transform(final_data["headline_text"])
 print("Transforming")
 tfidf_transformer = TfidfTransformer()
 tfidf_transformer.fit(news_vector)
-tfidf_transformer.transform(news_vector)
+news_vector = tfidf_transformer.transform(news_vector)
 
 movements = final_data.Average
 
@@ -58,11 +58,14 @@ print("splitting data")
 #split out test and train subsets:
 X_train, X_test, Y_train, Y_test = train_test_split(news_vector, movements, random_state=1)
 
+pickle.dump(Y_test,open("models/y_test.pkl","wb"))
+pickle.dump(X_test,open("models/x_test.pkl","wb"))
+
 for modelName in ["lbfgs", "sgd", "adam"]:
     for nMax in range(1,20):
 
         print(f"Model {modelName}, Max Iterations {nMax*10}")
-        clf = MLPRegressor(solver=modelName, alpha=1e-5,hidden_layer_sizes=(1000,20), random_state=1,verbose=True, max_iter=nMax*10)
+        clf = MLPRegressor(solver=modelName, alpha=1e-5,hidden_layer_sizes=(500,20), random_state=1,verbose=True, max_iter=nMax*10)
 
         print("starting regression")
         clf = clf.fit(X_train,Y_train)
